458075b6966756c9d1056ec02d4b62361da8d283
alexsander55455455-cmyk <alexsander55455455@gmail.com>
2026-07-04 14:40:06 +0700

    fix(render): center gun_brimstone in inventory and lower hand pose

--- src/main/java/com/hbm/render/item/weapon/ItemRenderBrimstone.java
+++ src/main/java/com/hbm/render/item/weapon/ItemRenderBrimstone.java
@@ -4,6 +4,7 @@ import com.hbm.main.ResourceManager;
 import com.hbm.render.item.TEISRBase;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.block.model.ItemCameraTransforms.TransformType;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import org.lwjgl.opengl.GL11;
@@ -16,18 +17,28 @@ public class ItemRenderBrimstone extends TEISRBase {
     }
 
     @Override
+    public boolean useRegistryPerspective(Item item) {
+        return true;
+    }
+
+    @Override
+    public boolean doNullTransform() {
+        return type == TransformType.GUI;
+    }
+
+    @Override
     public void renderByItem(ItemStack stack) {
         GL11.glPopMatrix();
         Minecraft.getMinecraft().getTextureManager().bindTexture(ResourceManager.brimstone_tex);
 
         switch (type) {
             case FIRST_PERSON_LEFT_HAND:
-                GL11.glTranslated(2.0D, 0.0D, 0.0D);
+                GL11.glTranslated(2.0D, -0.15D, 0.0D);
                 GL11.glRotated(-90.0D, 0.0D, 1.0D, 0.0D);
                 GL11.glRotated(40.0D, 1.0D, 0.0D, 0.0D);
                 break;
             case FIRST_PERSON_RIGHT_HAND:
-                GL11.glTranslated(-0.9D, -0.2D, 0.0D);
+                GL11.glTranslated(-0.9D, -0.35D, 0.0D);
                 GL11.glRotated(90.0D, 0.0D, 1.0D, 0.0D);
                 GL11.glRotated(40.0D, 1.0D, 0.0D, 0.0D);
                 break;
@@ -36,14 +47,15 @@ public class ItemRenderBrimstone extends TEISRBase {
             case GROUND:
             case THIRD_PERSON_LEFT_HAND:
             case THIRD_PERSON_RIGHT_HAND:
-                GL11.glTranslated(0.5D, -0.5D, -0.85D);
+                GL11.glTranslated(0.5D, -0.75D, -0.85D);
                 break;
             case GUI:
                 GlStateManager.enableLighting();
-                GL11.glScaled(0.5D, 0.5D, 0.5D);
-                GL11.glTranslatef(-0.2F, -0.1F, 0.0F);
-                GL11.glRotatef(-90.0F, 0.0F, 1.0F, 0.0F);
-                GL11.glRotatef(-40.0F, 1.0F, 0.0F, 0.0F);
+                GL11.glTranslated(0.5D, 0.42D, 0.5D);
+                GL11.glScaled(0.16D, 0.16D, 0.16D);
+                GL11.glTranslated(0.0D, -0.85D, 0.0D);
+                GL11.glRotated(-90.0D, 0.0D, 1.0D, 0.0D);
+                GL11.glRotated(-40.0D, 1.0D, 0.0D, 0.0D);
                 break;
             default:
                 break;
