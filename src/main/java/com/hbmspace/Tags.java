0f7d73de30cd29258cf62ec8d35c9f010b28c2ee
alexsander55455455-cmyk <alexsander55455455@gmail.com>
2026-07-19 02:42:26 +0700

    chore(metadata): update Reforged branding and attribution

--- src/main/java/com/hbmspace/Tags.java
+++ src/main/java/com/hbmspace/Tags.java
@@ -2,8 +2,8 @@ package com.hbmspace;
 
 public final class Tags {
     public static final String MODID = "hbmspace";
-    public static final String MODNAME = "HBM NTM X5687 Space";
-    public static final String VERSION = "0.2.0-alpha";
+    public static final String MODNAME = "HBM NTM Reforged Space";
+    public static final String VERSION = com.hbm.Tags.VERSION;
 
     private Tags() {
     }
